c1f6aaba008e6e7d38e3651602364d959bbdbfe8
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-09 23:03:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector);
+}
 
+function nestedTarget() {
+return document.getElementById('nested').querySelector('div.target');
+
+}
+
+function increaseRankBy(n) {
+
+
+}
+
+function deepestChild() {
+
+
+}
