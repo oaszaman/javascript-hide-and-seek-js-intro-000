ba62df48a1ad6f328a7ebf3fa0a3a1f9f22990a9
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-09 23:21:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector);
+}
 
+function nestedTarget() {
+return document.getElementById('nested').querySelector('div.target');
+
+}
+
+function increaseRankBy(n) {
+var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+
+}
+
+function deepestChild() {
+  var drag = document.getElementById('grand-node').querySelectorAll('div');
+  return drag[drag.length-1];
+}
